0407d6c3d14c0c703f4268dfdeca45ce5bafa27b
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2020-08-11 23:35:23 -0500

    add initial frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,49 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
+import Web3 from 'web3';
+import QuidditchPlayer from '../abis/QuidditchPlayer.json'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      account: '',
+      contract: null,
+      myPlayers: [],
+    };
+  }
+  async componentWillMount() {
+    await this.loadWeb3();
+    await this.loadBlockchainData();
+  }
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    } else {
+      window.alert('No-Ethereum browser detected. Install Metamask');
+    }
+  }
+  async loadBlockchainData() {
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = QuidditchPlayer.networks[networkId];
+    if (networkData) {
+      const abi = QuidditchPlayer.abi;
+      const address = networkData.address;
+      const contract = new web3.eth.Contract(abi, address);
+      this.setState({ contract: contract });
+    } else {
+      window.alert('Smart contract not deployed');
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,7 +54,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            CryptoQuidditch
           </a>
         </nav>
         <div className="container-fluid mt-5">
